9e05ab95a8d2c97b77ffa1496cae26e92f29eb04
gustavo <gustavo@ws03.valsol.net>
2018-03-17 18:53:56 -0300

    module name, author and website modified

--- website_favicon/__openerp__.py
+++ website_favicon/__openerp__.py
@@ -2,11 +2,11 @@
 # (c) 2015 Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
-    'name': 'Favicon',
+    'name': 'Favicon_valsol',
     'version': '8.0.1.0.0',
     'category': 'Website',
-    'author': 'Serv. Tecnol. Avanzados - Pedro M. Baeza',
-    'website': 'http://www.serviciosbaeza.com',
+    'author': 'VALSOL - GB',
+    'website': 'https://www.valsol.com',
     'depends': ['website'],
     'data': [
         'views/website_favicon.xml',
